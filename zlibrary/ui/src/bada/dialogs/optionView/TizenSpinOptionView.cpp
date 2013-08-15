c5902be87b7bb312182006b6533aebbb4983750d
Alex <temper8@gmail.com>
2013-08-16 03:56:08 +0400

    add TizenSpinOptionView

---
+++ zlibrary/ui/src/bada/dialogs/optionView/TizenSpinOptionView.cpp
@@ -0,0 +1,61 @@
+/*
+ * TizenSpinOptionView.cpp
+ *
+ *  Created on: Aug 16, 2013
+ *      Author: temper8
+ */
+
+#include "TizenSpinOptionView.h"
+
+
+using namespace Tizen::App;
+using namespace Tizen::Base;
+using namespace Tizen::Ui;
+using namespace Tizen::Ui::Controls;
+using namespace Tizen::Graphics;
+
+void TizenSpinOptionView::_createItem() {
+//	std::string myText = ((ZLStaticTextOptionEntry&)*myOption).initialValue();
+//	const char *text = myText.c_str();
+//	 myCaption = ZLOptionView::name()+ ": 123";
+	 myCaption.Format(30, L"%s", ZLOptionView::name().c_str());
+}
+
+void TizenSpinOptionView::_onAccept() const {
+
+}
+
+
+TableViewItem* TizenSpinOptionView::createTableViewItem(int itemWidth, int defaultItemHeight) {
+	TableViewAnnexStyle style = TABLE_VIEW_ANNEX_STYLE_NORMAL;
+	TableViewItem* pItem = new TableViewItem();
+	pItem->Construct(Dimension(itemWidth, 2*defaultItemHeight), style);
+
+	//String text;
+	//text.Format(30, L"t=%s", myCaption.c_str());
+
+	Label* pLabel = new Label();
+	pLabel->Construct(Rectangle(0, 0, itemWidth, defaultItemHeight), myCaption);
+	pItem->AddControl(pLabel);
+
+	Slider* __pSlider = new (std::nothrow) Slider();
+	Label*	pLabelMin = new (std::nothrow) Label();
+	pLabelMin->Construct(FloatRectangle(20.0f, 80.0f, 45.0f, 45.0f), L"12");
+
+	Label*	pLabelMax = new (std::nothrow) Label();
+	pLabelMax->Construct(FloatRectangle(650.0f, 80.0f, 45.0f, 45.0f), L"123");
+
+	__pSlider->Construct(FloatRectangle(55.0f, 0.0f, itemWidth-110.0f, 150.0f), BACKGROUND_STYLE_NONE, true, 0, 15);
+	__pSlider->SetValue(5);
+
+	pItem->AddControl(__pSlider);
+	pItem->AddControl(pLabelMin);
+	pItem->AddControl(pLabelMax);
+
+	pItem->SetIndividualSelectionEnabled(__pSlider, true);
+	pItem->SetIndividualSelectionEnabled(pLabelMin, true);
+	pItem->SetIndividualSelectionEnabled(pLabelMax, true);
+
+
+	return pItem;
+}
---
+++ zlibrary/ui/src/bada/dialogs/optionView/TizenSpinOptionView.h
@@ -0,0 +1,24 @@
+/*
+ * TizenSpinOptionView.h
+ *
+ *  Created on: Aug 16, 2013
+ *      Author: temper8
+ */
+
+#ifndef TIZENSPINOPTIONVIEW_H_
+#define TIZENSPINOPTIONVIEW_H_
+
+#include "ZLTizenOptionView.h"
+
+class TizenSpinOptionView: public ZLTizenOptionView {
+public:
+
+	TizenSpinOptionView(const std::string &name, const std::string &tooltip, ZLOptionEntry *option, ZLTizenDialogContent *tab, int row, int fromColumn, int toColumn) : ZLTizenOptionView(name, tooltip, option, tab, row, fromColumn, toColumn) {}
+	virtual Tizen::Ui::Controls::TableViewItem* createTableViewItem(int itemWidth, int defaultItemHeight);
+
+protected:
+	void _createItem();
+	void _onAccept() const;
+};
+
+#endif /* TIZENSPINOPTIONVIEW_H_ */
